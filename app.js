92458b20daf08fe61121530d41dadf3c21fa3237
erdemoden <erdemoden5@gmail.com>
2021-07-03 12:20:57 +0300

    oldu mu

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { request } = require('http');
 const cookieparser = require('cookie-parser');
 const app = express();
+app.listen(process.env.PORT||1998);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
@@ -12,7 +13,6 @@ app.get("/",(req,res)=>{
     res.render("index.ejs");
 });
 app.use('/',log);
-app.listen(process.env.PORT||1998);
 
 
 
